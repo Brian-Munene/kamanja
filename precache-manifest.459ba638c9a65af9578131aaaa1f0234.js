7dc28cdfbf15e9713d4011cac8769515d057a455
Brian-Munene <bmunene@fluidtechglobal.com>
2019-04-25 14:48:23 +0300

    Updates

---
+++ precache-manifest.459ba638c9a65af9578131aaaa1f0234.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8b105e0322ab53224c100d7cc20e1ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c67bc5ce49edbcaa8a8",
+    "url": "/static/css/2.1e66eaca.chunk.css"
+  },
+  {
+    "revision": "ea12c5deedbfc8c8fa01",
+    "url": "/static/css/main.ccd5deb5.chunk.css"
+  },
+  {
+    "revision": "9c67bc5ce49edbcaa8a8",
+    "url": "/static/js/2.b93e5efb.chunk.js"
+  },
+  {
+    "revision": "ea12c5deedbfc8c8fa01",
+    "url": "/static/js/main.30e7ff0e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cc97f65ce73d485b98b9d83f22a35c63",
+    "url": "/static/media/3D-Court-Yard-Front.cc97f65c.JPG"
+  },
+  {
+    "revision": "8fc745853c92ad4c35b8fa51d7c4215c",
+    "url": "/static/media/Astrol-2017.8fc74585.JPG"
+  },
+  {
+    "revision": "61cb168c1ee6b356c74cee3c91273efa",
+    "url": "/static/media/Crest-park.61cb168c.jpg"
+  },
+  {
+    "revision": "91dc61087b33ddb7649a400731d5461d",
+    "url": "/static/media/Godowns.91dc6108.jpg"
+  },
+  {
+    "revision": "f0f8c7ae657ec2f61090bb28fd7ce2fd",
+    "url": "/static/media/RIVER-LAND.f0f8c7ae.jpg"
+  },
+  {
+    "revision": "256b56d7d2d9e61a8da167c92047262d",
+    "url": "/static/media/StMary's.256b56d7.jpeg"
+  },
+  {
+    "revision": "dc3781319175180fe785396532c8d3b5",
+    "url": "/static/media/final_solid_gate.dc378131.jpg"
+  },
+  {
+    "revision": "0ca9e53a63c78e25792cf9b2cae52b38",
+    "url": "/static/media/multipurpose-hall.0ca9e53a.jpg"
+  },
+  {
+    "revision": "64dd890d1c769eca357485edbda46c9f",
+    "url": "/static/media/stunning-homes.64dd890d.jpg"
+  },
+  {
+    "revision": "31ce17ef19f1645f671dd9cb86025325",
+    "url": "/static/media/town-houses.31ce17ef.bmp"
+  }
+]);
