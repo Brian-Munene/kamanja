606c30dcdad384ef3c66477330d6b20e2761b577
Brian-Munene <bmunene@fluidtechglobal.com>
2019-04-25 14:37:11 +0300

    Updates

---
+++ precache-manifest.d9981f15d4ca02b17780380532d3f991.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b6b413ce7793b03252188a6184ee8b7",
+    "url": "kamanjateamdesigns.com/index.html"
+  },
+  {
+    "revision": "9c67bc5ce49edbcaa8a8",
+    "url": "kamanjateamdesigns.com/static/css/2.1e66eaca.chunk.css"
+  },
+  {
+    "revision": "8a06d42ca6478f56ac97",
+    "url": "kamanjateamdesigns.com/static/css/main.36ffd7ce.chunk.css"
+  },
+  {
+    "revision": "9c67bc5ce49edbcaa8a8",
+    "url": "kamanjateamdesigns.com/static/js/2.b93e5efb.chunk.js"
+  },
+  {
+    "revision": "8a06d42ca6478f56ac97",
+    "url": "kamanjateamdesigns.com/static/js/main.9623340a.chunk.js"
+  },
+  {
+    "revision": "52eadd0b2ecf5f2eddf1",
+    "url": "kamanjateamdesigns.com/static/js/runtime~main.d5bf6316.js"
+  },
+  {
+    "revision": "cc97f65ce73d485b98b9d83f22a35c63",
+    "url": "kamanjateamdesigns.com/static/media/3D-Court-Yard-Front.cc97f65c.JPG"
+  },
+  {
+    "revision": "8fc745853c92ad4c35b8fa51d7c4215c",
+    "url": "kamanjateamdesigns.com/static/media/Astrol-2017.8fc74585.JPG"
+  },
+  {
+    "revision": "61cb168c1ee6b356c74cee3c91273efa",
+    "url": "kamanjateamdesigns.com/static/media/Crest-park.61cb168c.jpg"
+  },
+  {
+    "revision": "91dc61087b33ddb7649a400731d5461d",
+    "url": "kamanjateamdesigns.com/static/media/Godowns.91dc6108.jpg"
+  },
+  {
+    "revision": "f0f8c7ae657ec2f61090bb28fd7ce2fd",
+    "url": "kamanjateamdesigns.com/static/media/RIVER-LAND.f0f8c7ae.jpg"
+  },
+  {
+    "revision": "256b56d7d2d9e61a8da167c92047262d",
+    "url": "kamanjateamdesigns.com/static/media/StMary's.256b56d7.jpeg"
+  },
+  {
+    "revision": "dc3781319175180fe785396532c8d3b5",
+    "url": "kamanjateamdesigns.com/static/media/final_solid_gate.dc378131.jpg"
+  },
+  {
+    "revision": "0ca9e53a63c78e25792cf9b2cae52b38",
+    "url": "kamanjateamdesigns.com/static/media/multipurpose-hall.0ca9e53a.jpg"
+  },
+  {
+    "revision": "64dd890d1c769eca357485edbda46c9f",
+    "url": "kamanjateamdesigns.com/static/media/stunning-homes.64dd890d.jpg"
+  },
+  {
+    "revision": "31ce17ef19f1645f671dd9cb86025325",
+    "url": "kamanjateamdesigns.com/static/media/town-houses.31ce17ef.bmp"
+  }
+]);
